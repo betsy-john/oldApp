7eefdc8d4a438c8ff7fc4eb9fc4963ebde305512
betsy <betsy@v2blr0027>
2018-04-27 18:09:06 +0530

    terms and policy

--- app.js
+++ app.js
@@ -67,4 +67,9 @@ angular.module('oldApp').config(function ($stateProvider,$urlRouterProvider) {
         templateUrl: 'donateUs/confirm.html', 
         controller: 'appointment as vm'     
     })
+    .state('privacyanduse', {
+        url: '/privacyanduse',
+        templateUrl: 'terms-and-policy/termAndService.html', 
+           
+    })
 });
