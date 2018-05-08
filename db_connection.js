df109919338f3dc14aede52a8179a5f682ee0552
betsy <betsy@v2blr0027>
2018-05-08 11:44:16 +0530

    setup

--- db_connection.js
+++ db_connection.js
@@ -5,7 +5,7 @@ var knex = require('knex')({
       host : '127.0.0.1',
       user : 'your_database_user',
       password : 'your_database_password',
-      database : 'myapp_test',
+      database : 'test',
       debug:true
     }
   });
